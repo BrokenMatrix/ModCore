7843b47d67976a1db9875c328d56789956d8d509
Thomas A White <thomascraft333@gmail.com>
2017-08-07 22:50:46 -0400

    Hopefully fixed colors

--- ModCore/src/com/brokenmatrix/modcore/blocks/CustomColorableBlock.java
+++ ModCore/src/com/brokenmatrix/modcore/blocks/CustomColorableBlock.java
@@ -1,34 +1,23 @@
 package com.brokenmatrix.modcore.blocks;
 
-import org.bukkit.DyeColor;
 import org.bukkit.Material;
-import org.bukkit.block.BlockState;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.material.Colorable;
-import org.bukkit.material.MaterialData;
 
 import com.brokenmatrix.modcore.items.drops.CustomDrop;
 
 public class CustomColorableBlock extends CustomBlock
 {
-	private DyeColor color;
+	private byte color;
 	
-	public CustomColorableBlock(String name, Material material, DyeColor color, CustomDrop... drops)
+	public CustomColorableBlock(String name, Material material, byte color, CustomDrop... drops)
 	{
 		super(name, material, drops);
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onPlace(BlockPlaceEvent e)
 	{
-		BlockState state = e.getPlayer().getWorld().getBlockAt(e.getBlock().getLocation()).getState();
-		MaterialData data = state.getData();
-		if (data instanceof Colorable)
-		{
-			Colorable c = (Colorable) data;
-			c.setColor(color);
-		}
-		state.setData(data);
-		state.update();
+		e.getPlayer().getWorld().getBlockAt(e.getBlock().getLocation()).getState().getData().setData(color);
 	}
 }
--- ModCore/src/com/brokenmatrix/modcore/items/CustomColorableBlockItem.java
+++ ModCore/src/com/brokenmatrix/modcore/items/CustomColorableBlockItem.java
@@ -1,39 +1,33 @@
 package com.brokenmatrix.modcore.items;
 
-import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.Colorable;
 import org.bukkit.material.MaterialData;
 
 import com.brokenmatrix.modcore.blocks.CustomBlock;
 
 public class CustomColorableBlockItem extends CustomBlockItem
 {
-	private DyeColor color;
+	private byte color;
 	
-	public CustomColorableBlockItem(String name, Material base, DyeColor color, CustomBlock block, boolean dropsSelf, int maxStackSize, String... lore)
+	public CustomColorableBlockItem(String name, Material base, byte color, CustomBlock block, boolean dropsSelf, int maxStackSize, String... lore)
 	{
 		super(name, base, block, dropsSelf, maxStackSize, lore);
 	}
 	
-	public CustomColorableBlockItem(String name, Material base, DyeColor color, CustomBlock block, boolean dropsSelf, String... lore)
+	public CustomColorableBlockItem(String name, Material base, byte color, CustomBlock block, boolean dropsSelf, String... lore)
 	{
 		this(name, base, color, block, dropsSelf, 1, lore);
 	}
 	
+	@SuppressWarnings("deprecation")
 	public ItemStack getItemStack(int amount)
 	{
 		ItemStack stack = super.getItemStack(amount);
 		
 		MaterialData data = stack.getData();
-		if (data instanceof Colorable)
-		{
-			Colorable c = (Colorable) data;
-			c.setColor(color);
-			
-			stack.setData((MaterialData) c);
-		}
+		data.setData(color);
+		stack.setData(data);
 		
 		return stack;
 	}
