12e61d233adf6b741870247915f8153bfc4a7dd2
Thomas A White <thomascraft333@gmail.com>
2017-08-08 10:30:04 -0400

    Added dupe command

--- ModCore/src/com/brokenmatrix/modcore/CommandModCore.java
+++ ModCore/src/com/brokenmatrix/modcore/CommandModCore.java
@@ -5,6 +5,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.brokenmatrix.modcore.items.CustomItem;
@@ -39,50 +41,11 @@ public class CommandModCore implements CommandExecutor
 			}
 			else if (args[0].equals("give"))
 			{
-				if (sender.hasPermission("modcore.give"))
-				{
-					if (args.length == 4)
-					{
-						Player player = Bukkit.getPlayer(args[1]);
-						
-						if (player != null)
-						{
-							CustomItem item = CustomItems.GetItemUnsafe(args[2]);
-							
-							if (item != null)
-							{
-								int amount = Integer.parseInt(args[3]);
-								
-								if (amount > 0)
-								{
-									player.getInventory().addItem(item.getItemStack(amount));
-									
-									message = ChatColor.GREEN + "Gave " + player.getName() + " " + amount + " of " + item.getName() + "!";
-								}
-								else
-								{
-									message = ChatColor.RED + "The item amount must be greater than zero.";
-								}
-							}
-							else
-							{
-								message = ChatColor.RED + "'" + args[2] + "' is not a valid item! Do not use spaces or color codes.";
-							}
-						}
-						else
-						{
-							message = ChatColor.RED + args[1] + " is not a valid player!";
-						}
-					}
-					else
-					{
-						message = ChatColor.RED + "Give needs 3 arguments, <player>, <item> and <amount>!";
-					}
-				}
-				else
-				{
-					message = ChatColor.RED + "You aren't allow to cheat!";
-				}
+				message = giveCommand(sender, args);
+			}
+			else if (args[0].equals("dupe"))
+			{
+				message = dupeCommand(sender);
 			}
 		}
 		if (message == null)
@@ -95,6 +58,76 @@ public class CommandModCore implements CommandExecutor
 		return true;
 	}
 	
+	private String dupeCommand(CommandSender sender)
+	{
+		if (sender.hasPermission("modcore.give"))
+		{
+			PlayerInventory inventory = Bukkit.getPlayer(sender.getName()).getInventory();
+			
+			for (ItemStack s : inventory.getStorageContents())
+			{
+				CustomItem i = Helper.GetItem(s);
+				
+				if (i != null)
+				{
+					inventory.addItem(i.getItemStack(s.getAmount()));
+				}
+			}
+			
+			return ChatColor.GREEN + "Duplicated all custom items in your inventory!";
+		}
+		
+		return ChatColor.RED + "That would be cheating!";
+	}
+	
+	private String giveCommand(CommandSender sender, String[] args)
+	{
+		if (sender.hasPermission("modcore.give"))
+		{
+			if (args.length == 4)
+			{
+				Player player = Bukkit.getPlayer(args[1]);
+				
+				if (player != null)
+				{
+					CustomItem item = CustomItems.GetItemUnsafe(args[2]);
+					
+					if (item != null)
+					{
+						int amount = Integer.parseInt(args[3]);
+						
+						if (amount > 0)
+						{
+							player.getInventory().addItem(item.getItemStack(amount));
+							
+							return ChatColor.GREEN + "Gave " + player.getName() + " " + amount + " of " + item.getName();
+						}
+						else
+						{
+							return ChatColor.RED + "The item amount must be greater than zero.";
+						}
+					}
+					else
+					{
+						return ChatColor.RED + "'" + args[2] + "' is not a valid item! Do not use spaces or color codes.";
+					}
+				}
+				else
+				{
+					return ChatColor.RED + args[1] + " is not a valid player!";
+				}
+			}
+			else
+			{
+				return ChatColor.RED + "Give needs 3 arguments, <player>, <item> and <amount>!";
+			}
+		}
+		else
+		{
+			return ChatColor.RED + "You aren't allow to cheat!";
+		}
+	}
+	
 	private String getHelpMessage()
 	{
 		StringBuilder message = new StringBuilder();
@@ -103,6 +136,7 @@ public class CommandModCore implements CommandExecutor
 		message.append("   /mc help - Displays the help page\n");
 		message.append("   /mc info - Displays information about the information.\n");
 		message.append("   /mc give <player> <item> <amount> - Gives a player\n   the specified custom item\n");
+		message.append("   /mc dupe - Duplicates all custom items in your inventory");
 		
 		return message.toString();
 	}
